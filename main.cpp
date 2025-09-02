fc4ab43d7b08be285cdb9b08240cb5db91194123
Germano Corrêa <germanobruscato@gmail.com>
2025-09-02 17:51:59 -0300

    Initial dotfiles, c++ clion project

---
+++ main.cpp
@@ -0,0 +1,7 @@
+// pixel por metro é a primeira info no arquivo de paths
+// dps, cada linha é uma entidade e o 1o nro é a qtd de frames em que ela aparece
+// dps cada valor é uma tupla de x,y,f sendo f o frame
+// o arquivo representa um frame
+
+int main() {
+}
